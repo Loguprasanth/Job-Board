3ace9ff0a30ec87f2fcfb61a9163ee8ecbc56ca4
Logu-prasanth <loguprasanth.2005044@srec.ac.in>
2025-08-04 22:52:45 +0530

    Removed unwanted code

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,23 +1,3 @@
 export const jobs = [
-  {
-    id: 1,
-    title: 'Senior Frontend Developer',
-    company: 'TechCorp Inc.',
-    type: 'Full-time',
-    mode: 'Remote',
-    date: '2 days ago',
-    description:
-      "We're looking for an experienced frontend developer with expertise in React, TypeScript, and modern CSS frameworks...",
-  },
-  {
-    id: 2,
-    title: 'UX/UI Designer',
-    company: 'DesignHub',
-    type: 'Contract',
-    mode: 'Hybrid',
-    date: '1 week ago',
-    description:
-      'Join our creative team as a UX/UI Designer to create beautiful, intuitive interfaces for our clients...',
-  },
-  // Add more jobs...
+  
 ];
